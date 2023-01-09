be487537fd7beddd8fca66e810a48c32e59f5f28
samuii1 <samehangel44@gmail.com>
2023-01-09 16:39:57 +0200

    adding main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from help_cog import help_cog
+from music_cog import music_cog
+import asyncio
+import os,sys
+
+class MyBot(commands.Bot):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+        
+try:
+    print(os.environ['DiscordBotToken'])
+except:
+    print("Need to specify DiscordBotToken using an Environment Variable \n"+
+          "On linux that can be achieved like this\n"+
+          "export DiscordBotToken=\"xxxx\""
+          )
+    sys.exit(1)
+
+
+        
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+loop = asyncio.get_event_loop()
+
+bot = MyBot(command_prefix=".",intents=intents)
+
+bot.remove_command('help')
+loop.run_until_complete(bot.add_cog(help_cog(bot)))
+loop.run_until_complete(bot.add_cog(music_cog(bot)))
+
+bot.run(os.environ["DiscordBotToken"])
